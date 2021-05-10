d21535585188a9dee9e6b95d5c04e7b7b7af020f
German Renato Cobian <germancobian@hotmail.com>
2021-05-10 14:17:14 -0700

    Added buttons for befriending and accepting or rejecting friend request

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -6,10 +6,10 @@ module UserHelper
   def get_is_my_friend_or_request(user)
     return if owner(user)
 
-    return render html: '<i class="fas fa-heart"></i>'.html_safe if current_user.friend?(user)
+    return render html: '<button type="button" class="bttn-friend"><i class="fas fa-heart"></i><span> Accepted</span></button>'.html_safe if current_user.friend?(user)
 
     if current_user.pending_friends.include?(user) || current_user.friend_requests.include?(user)
-      render html: '<i class="fas fa-heartbeat"></i> <span>Pending</span>'.html_safe
+      render html: '<button type="button" class="bttn-friend"><i class="fas fa-heartbeat"></i><span> Pending</span></button>'.html_safe
     else
       link_to('Add Friend?', user_friendships_path(user_id: user.id), method: :post, class: '')
     end
@@ -19,13 +19,13 @@ module UserHelper
     actual_user = User.find_by(id: params[:id])
     return unless current_user == actual_user
 
-    link_to('Accept?', user_friendship_path(user_id: friendship.id), method: :put, class: '')
+    link_to('Accept?', user_friendship_path(user_id: friendship.id), method: :put, class: 'tag-color')
   end
 
   def reject_friendship(friendship)
     actual_user = User.find_by(id: params[:id])
     return unless current_user == actual_user
 
-    link_to('Reject?', user_friendship_path(user_id: friendship.id), method: :delete, class: '')
+    link_to('Reject?', user_friendship_path(user_id: friendship.id), method: :delete, class: 'tag-color')
   end
 end
