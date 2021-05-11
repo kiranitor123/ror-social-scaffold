6f40c64be7aff9d0136df35142753446a6dcac6e
kiranitor123 <rolandoalvarezfaye@gmail.com>
2021-05-11 10:49:28 -0400

    Update user spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,6 +6,5 @@ RSpec.describe User, type: :model do
     it { should have_many(:comments) }
     it { should have_many(:likes) }
     it { should have_many(:friendships) }
-    it { should have_many(:inverse_friendships) }
   end
 end
