177f8d0b5dac65626dc948fc69a949ee36e2decd
kiranitor123 <rolandoalvarezfaye@gmail.com>
2021-05-11 12:05:26 -0400

    Fix linter problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,13 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = begin
+    @user = 
+    begin
       User.find(params[:id])
     rescue StandardError
       current_user
     end
+    
     @posts = @user.posts.ordered_by_most_recent
   end
 end
