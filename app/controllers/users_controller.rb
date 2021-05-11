20d40f12c7d349472b1cc9ceadbc78f74a72ef17
kiranitor123 <rolandoalvarezfaye@gmail.com>
2021-05-11 12:06:24 -0400

    Fix it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = 
-    begin
+    @user = begin
       User.find(params[:id])
     rescue StandardError
       current_user
     end
-    
+
     @posts = @user.posts.ordered_by_most_recent
   end
 end
